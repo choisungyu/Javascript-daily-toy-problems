14060c0a75ff393ae14d8c48a8cc66c06913aab0
Dev Paul <48423344+choisungyu@users.noreply.github.com>
2020-05-14 23:27:17 +0900

    Create toy-01.js
    다시 풀어볼 것...

---
+++ toy-01.js
@@ -0,0 +1,19 @@
+const rockPaperScissors = function() {
+    // TODO: Your code here!
+    // 세 판 가위바위보 낼 경우의 수 : 27
+    let inner = ['rock', 'rock', 'rock'];
+    //inner[0] , inner[1], inner[2]
+    // inner[0] = 'rock' ,
+    inner[0] = 'rock'
+    inner[1] = 'scissors'
+    inner[2] = 'paper'
+
+    let outer = [];
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+
+        }
+        return outer
+    }
+};
