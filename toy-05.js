f1c014838229e60bc19a7a24a39c2beea7197a7d
Dev Paul <48423344+choisungyu@users.noreply.github.com>
2020-05-24 21:05:30 +0900

    Create toy-05.js
    1. 첫번째로 [ 24.3, 23, 9, 3, 3, 24.7, 25, 100, 100 ] 값이 나옴.24.7 이 25 앞에 멈춤. 첫번째 sort 끝남
    2. 두번째로 [ 23, 9, 3, 3, 24.3, 24.7, 25, 100, 100 ] 값이 나옴 24.3 이 24.7 앞에 멈춤. 두번째 sort 끝.
    ...
    flag가 true로 갈 때, bubbleSort로 가는데 갈 때 마다, flag는 false 로 감?!
    그러다가 정렬 할 게 없어지면, flag는 그대로 false가 되고 return array로 종료를 한다.

---
+++ toy-05.js
@@ -0,0 +1,20 @@
+const bubbleSort = function(array) {
+    // TODO: Your code here!
+    // 0 1, 2
+    let flag = false;
+
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            [array[i], array[i + 1]] = [array[i + 1], array[i]];
+            flag = true
+        }
+    }
+    if (flag) {
+        console.log(array);
+        return bubbleSort(array);
+    }
+    // console.log(array);
+    
+    return array;
+};
+bubbleSort([24.7, 24.3, 23, 9, 3, 3, 100, 25, 100])
