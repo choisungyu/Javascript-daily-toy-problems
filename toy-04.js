31042ac51998c7c36517d3a180cf43f13f0c2f96
Dev Paul <48423344+choisungyu@users.noreply.github.com>
2020-05-24 20:42:32 +0900

    Update toy-04.js
     const fib = [];  fib.push(0, 1);
        const fib = [0,1];

--- toy-04.js
+++ toy-04.js
@@ -8,3 +8,13 @@ const nthFibonacci = function(n) {
   }
   return fib[n];
 };
+
+// #2
+function iterativeFibo(n) {
+    const cache = [0,1];
+    for(let i = 2 ; i <= n ; i++) {
+        cache[i] = cache[i-2] + cache[i-1];
+    }
+    return cache[n];
+}
+console.log(iterativeFibo(20));
